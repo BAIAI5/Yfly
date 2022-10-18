f9f116552fbb5dfa95e7182fee636649fcac408a
BAIAI5 <Wang2OOO126@163.com>
2022-10-19 01:51:43 +0800

    add server code

--- pingpong/pingpong_server.cpp
+++ pingpong/pingpong_server.cpp
@@ -1,1 +1,45 @@
 #include "pingpong_server.h"
+
+#include <cstdio>
+
+void Server::init(int port)
+{
+    // init server socket
+    struct sockaddr_in ServerAddr;
+    if ((ServerSock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        ErrorHandling("socket() error");
+    
+    ServerAddr.sin_family = AF_INET;
+    ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    ServerAddr.sin_port = htons(port);
+
+    if (bind(ServerSock, (struct sockaddr *)&ServerAddr, sizeof(ServerAddr)) < 0)
+        ErrorHandling("bind() error");
+    
+    if (listen(ServerSock, 1024) < 0)
+        ErrorHandling("listen() error");
+
+    ClientSock = (int *)malloc(sizeof(int) * MaxConnection);
+
+    // init epoll fd
+    struct epoll_event event;
+    event.events = EPOLLIN;
+    event.data.fd = ServerSock;
+    EpollFd = epoll_create(EpollSize);
+    EpollEvents = (struct epoll_event *)malloc(sizeof(epoll_event) * EpollSize);
+    epoll_ctl(EpollFd, EPOLL_CTL_ADD, ServerSock, &event);
+}
+
+void Server::connect()
+{
+    
+}
+
+
+
+void ErrorHandling(const char *ErrorMsg)
+{
+    fputs(ErrorMsg, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
--- pingpong/pingpong_server.h
+++ pingpong/pingpong_server.h
@@ -1,19 +1,28 @@
 #ifndef PINGPONG_SERVER_H
 #define PINGPONG_SERVER_H
 
+#include <map>
 #include <sys/epoll.h>
 #include <sys/timerfd.h>
 #include <sys/socket.h>
-#include <map>
+#include <arpa/inet.h>
 
 class Server
 {
 public:
-    void init();
-    void run();
+    ~Server()
+    {
+        free(ClientSock);
+    }
+    void init(int port);
+    void connect();
 private:
-    int ServerSock, EpollFd;
+    int ServerSock{}, EpollFd{};
     int ConnectionCnt{};
+    int *ClientSock = NULL;
+    const int MaxConnection = 1000;
+    const int EpollSize = 2000;
+    struct epoll_event *EpollEvents;
     std::map<int, int> Time2Client;
 };
 
